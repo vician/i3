c0d04b0cd35dea7eb16ee4855527c7cb37951235
Martin Vician <martin.vician@nic.cz>
2016-01-06 16:42:23 +0100

    background via feh, spaces to tabs

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ else
 fi
 
 # Additional packages
-additionals=("i3lock" "i3blocks" "i3status" "xautolock" "acpi" "lm-sensors" "terminator" "dunst")
+additionals=("i3lock" "i3blocks" "xautolock" "acpi" "lm-sensors" "terminator" "dunst" "feh" )
 echo "Checking additional packages (${additional[*]}"
 for additional in ${additionals[@]}; do
 	echo -n "- $additional"
