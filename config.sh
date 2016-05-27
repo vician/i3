9c25b02349297d1a0f76663de38aec065158daea
Martin Vician <martin.vician@nic.cz>
2016-05-27 10:36:47 +0200

    different workspaces

--- config.sh
+++ config.sh
@@ -28,32 +28,50 @@ cp $CONFIG $FINAL_CONFIG
 
 
 if [ $HOSTNAME == "remus" ]; then
-	OUTPUT_DEFINITION='set $min LVDS1\nset $mex HDMI3'
-	TRAY_OUTPUT=''
+	#OUTPUT_DEFINITION='set $min LVDS1\nset $mex HDMI3'
+	sh_w0mon="LVDS1"
+	sh_w1mon="HDMI3"
+	sh_wterms="HDMI3"
+	sh_w7mon="LVDS1"
+	sh_w8mon="HDMI3"
+	sh_w9mon="LVDS1"
+	sh_w10mon="HDMI3"
+	sh_w11mon="LVDS1"
+	sh_w12mon="LVDS1"
+	sh_w13mon="HDMI3"
+
 elif [ $HOSTNAME == "pete" ]; then
-	OUTPUT_DEFINITION='set $min HDMI-1\nset $mex eDP-1'
-	#TRAY_OUTPUT='tray_output $min'
-	TRAY_OUTPUT=''
+	#OUTPUT_DEFINITION='set $min HDMI-1\nset $mex eDP-1'
+	sh_w0mon="eDP-1"
+	sh_w1mon="eDP-1"
+	sh_wterms="eDP-1"
+	sh_w7mon="eDP-1"
+	sh_w8mon="eDP-1"
+	sh_w9mon="eDP-1"
+	sh_w10mon="eDP-1"
+	sh_w11mon="HDMI-1"
+	sh_w12mon="HDMI-1"
+	sh_w13mon="eDP-1"
 else
-	echo "Unknown hostname!"
+	echo "Unknosh_wn hostname!"
 	exit 1
 fi
 
 
-if [ "$OUTPUT_DEFINITION" ]; then
-	# Add new lines behind OutputDefiniton
-	sed -i "/OutputDefiniton/a $OUTPUT_DEFINITION" $FINAL_CONFIG
-else
-	echo "Skipping OutputDefiniton..."
-fi
-
-
-if [ "$TRAY_OUTPUT" ]; then
-	# Add new line beind TrayOutput
-	sed -i "/TrayOutput/a $TRAY_OUTPUT"  $FINAL_CONFIG
-else
-	echo "Skipping TrayOutput..."
-fi
+variables=("sh_w0mon" "sh_w1mon" "sh_wterms" "sh_w7mon" "sh_w8mon" "sh_w9mon" "sh_w10mon" "sh_w11mon" "sh_w12mon" "sh_w13mon")
+for variable in ${variables[@]}; do
+	echo -n "- variable $variable = ${!variable} - "
+	if [ "${!variable}" ] ; then
+		sed -i "s/$variable/${!variable}/g" $FINAL_CONFIG
+		if [ $? -ne 0 ]; then
+			echo "FAILED!"
+		else
+			echo "OK"
+		fi
+	else
+		echo "EMPTY!"
+	fi
+done
 
 # Add do not change this file
 sed -i "1s/^/##################################\n### DO NOT CHANGE THIS FILE!!! ###\n##################################\n\n/" $FINAL_CONFIG
