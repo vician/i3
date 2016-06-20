ab50675384bbac211184b1255605d90d63c7d5ff
Martin Vician <martin.vician@nic.cz>
2016-06-20 11:08:19 +0200

    testing slock

--- lock.sh
+++ lock.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-pgrep i3lock
-if [ $? -eq 0 ]; then
-	echo "already locked"
-	exit 0;
-fi
+#LOCK="i3lock"
+#LOCK_CMD="-e --color 000000"
+LOCK="slock"
+LOCK_CMD=""
 
-#if [ $# -eq 0 ]; then
-#	~/.i3/yubi/approved.sh
-#	if [ $? -eq 0 ]; then
-#		i3-nagbar -t warning -m 'WARNING: Inserted security key, are you sure that you want to lock computer manually?!' -b 'Yes, lock' '/home/martin/.i3/lock.sh nosmartcard'
-#		exit 0
-#	fi
-#else
-#	if [ "$1" = "nosmartcard" ]; then
-#		touch ~/.i3/yubi/force.lock
-#	fi
+pkill $LOCK #Always kill previous locked
+#pgrep $LOCK
+#if [ $? -eq 0 ]; then
+#	echo "already locked"
+#	exit 0;
 #fi
 
-grep gnome-mplayer ~/.i3/player
-if [ $? -ne 0 ]; then
-	~/.i3/player-control stop # Stop possible playing
-fi
-~/.i3/layout-default.sh
-i3lock -e --color 000000 # lock
+~/.i3/player-control stop # Stop possible playing
+~/.i3/layout-default.sh # Change keyboard to English
+# lock
+$LOCK $LOCK_CMD
