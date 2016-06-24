10bc32dc7f048ff816cc61f237e2990e03d93f76
Martin Vician <martin.vician@nic.cz>
2016-06-24 14:01:52 +0200

    parallel running

--- work.sh
+++ work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pgrep pidgin 1>/dev/null || pidgin
-pgrep thunderbird 1>/dev/null || thunderbird
-ps faux | grep keepass2 | grep -v grep 1>/dev/null || keepass2
+pgrep pidgin 1>/dev/null || i3-msg "exec pidgin"
+pgrep thunderbird 1>/dev/null || i3-msg "exec thunderbird"
+ps faux | grep keepass2 | grep -v grep 1>/dev/null || i3-msg "exec keepass2"
 pgrep spotify 1>/dev/null || i3-msg "workspace =♪; exec spotify" 1>/dev/null
 pgrep chromium 1>/dev/null || i3-msg "workspace 1🌎; exec chromium-browser" 1>/dev/null
