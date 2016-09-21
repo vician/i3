1fe2c39f4af7053159d1a18e25a8755797fa97c8
Martin Vician <info@vician.cz>
2016-09-21 17:33:25 +0200

    disable already locked

--- lock.sh
+++ lock.sh
@@ -5,7 +5,7 @@ LOCK_CMD="-d -f -e --color 000000"
 #LOCK="slock"
 #LOCK_CMD="/usr/bin/xset dpms force off"
 
-pkill $LOCK #Always kill previous locked
+#pkill $LOCK #Always kill previous locked
 #pgrep $LOCK
 #if [ $? -eq 0 ]; then
 #	echo "already locked"
