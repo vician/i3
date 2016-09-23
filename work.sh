f3e5644bfd1aa2f27154aa24876501c52263571c
Martin Vician <info@vician.cz>
2016-09-23 15:21:50 +0200

    added mattermost

--- work.sh
+++ work.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-pgrep pidgin 1>/dev/null || i3-msg "exec pidgin"
-pgrep thunderbird 1>/dev/null || i3-msg "exec thunderbird"
-pgrep keepassx2 1>/dev/null | i3-msg "exec keepassx2"
+pgrep pidgin 1>/dev/null || i3-msg "exec pidgin" 1>/dev/null
+pgrep Mattermost 1>/dev/null || i3-msg "exec mattermost" 1>/dev/null
+pgrep thunderbird 1>/dev/null || i3-msg "exec thunderbird" 1>/dev/null
+pgrep keepassx2 1>/dev/null | i3-msg "exec keepassx2" 1>/dev/null
 pgrep spotify 1>/dev/null || i3-msg "exec spotify" 1>/dev/null
 pgrep chromium 1>/dev/null || i3-msg "workspace 1w; exec chromium" 1>/dev/null
