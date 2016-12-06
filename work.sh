f36559f991a58e481404e146ab0785899aaeb3c5
Martin Vician <info@vician.cz>
2016-12-06 08:59:40 +0100

    added telegram

--- work.sh
+++ work.sh
@@ -6,3 +6,4 @@ pgrep thunderbird 1>/dev/null || i3-msg "exec thunderbird" 1>/dev/null
 pgrep keepassx2 1>/dev/null | i3-msg "exec keepassx2" 1>/dev/null
 pgrep spotify 1>/dev/null || i3-msg "exec spotify" 1>/dev/null
 pgrep chromium 1>/dev/null || i3-msg "workspace 1w; exec chromium" 1>/dev/null
+pgrep telegram 1>/dev/null || i3-msg "exec telegram-desktop" 1>/dev/null
